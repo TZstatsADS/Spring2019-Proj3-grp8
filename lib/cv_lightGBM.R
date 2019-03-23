9e018919c622b4c0ec01c82bb73761482f714f90
Yujie Wang <yujiewang1110@gmail.com>
2019-03-23 18:21:55 -0400

    save

--- lib/cv_lightGBM.R
+++ lib/cv_lightGBM.R
@@ -2,7 +2,7 @@
 ### Cross Validation for light GBM ###
 ######################################
 
-cv.lgb <- function(X.train, y.train, d, nr, K){
+cv.lgb <- function(X.train, y.train, nr, lr, K){
   
   n <- dim(y.train)[1]
   n.fold <- floor(n/K)
@@ -15,7 +15,7 @@ cv.lgb <- function(X.train, y.train, d, nr, K){
     test.data <- X.train[s == i, ,]
     test.label <- y.train[s == i, ,]
     
-    par <- list(depth=d, nr = nr)
+    par <- list(nr = nr, lr =lr)
     fit <- train.lgb(train.data, train.label, par)
     pred <- test.lgb(fit, test.data)  
     cv.lgb.error[i] <- mean((pred - test.label)^2)  
@@ -25,11 +25,21 @@ cv.lgb <- function(X.train, y.train, d, nr, K){
 }
 
 
-nrseq <- seq(200, 200, 10)
+nrseq <- seq(10, 300, 10)
 err_cv_lgb <- cbind(nrseq, array(dim=c(length(nrseq), 2)))
 colnames(err_cv_lgb) <- c("Num_iterations", "mean_cv_error", "sd_cv_error")
 for (i in 1:length(nrseq)){
-  err_cv_lgb[i,] <- cv.lgb(dat_train$feature, dat_train$label, nr = nrseq[i], d = 50, K = 5)
+  err_cv_lgb[i,2:3] <- cv.lgb(dat_train$feature, dat_train$label, nr = nrseq[i], , K = 5)
 }
 
 save(err_cv_lgb, file="../output/err_cv_lgb.RData")
+
+
+lrseq <- seq(0.01, 0.2, 0.01)
+err_cv_lgb <- cbind(lrseq, array(dim=c(length(lrseq), 2)))
+colnames(err_cv_lgb) <- c("Learning Rate", "mean_cv_error", "sd_cv_error")
+for (i in 1:length(lrseq)){
+  err_cv_lgb[i,2:3] <- cv.lgb(dat_train$feature, dat_train$label, nr = 200,  lr = lrseq[i], K = 5)
+}
+
+save(err_cv_lgb, file="../output/err_cv_lgb_1.RData")
--- lib/train_lightgbm.R
+++ lib/train_lightgbm.R
@@ -19,11 +19,8 @@ train.lgb <- function(dat_train, label_train, par=NULL){
   modelList <- list()
   
   ### Train with gradient boosting model
-  if(is.null(par)){
-    depth <- 3
-  } else {
-    depth <- par$depth
-  }
+    nr <- par$nr
+    lr <- par$lr
   
   ### the dimension of response arrat is * x 4 x 3, which requires 12 classifiers
   ### this part can be parallelized
@@ -33,9 +30,9 @@ train.lgb <- function(dat_train, label_train, par=NULL){
     c2 <- (i-c1) %/% 4 + 1
     featMat <- dat_train[, , c2]
     labMat <- label_train[, c1, c2]
-    fit_lgb <- lightgbm(data=featMat, label=labMat, objective="regression")
+    fit_lgb <- lightgbm(data=featMat, label=labMat, objective="regression", nrounds = nr, learning_rate = lr)
     #best_iter <- gbm.perf(fit_gbm, method="OOB", plot.it = FALSE)
-    modelList[[i]] <- list(fit=fit_gbm, iter=best_iter)
+    modelList[[i]] <- list(fit=fit_lgb, iter=nr)
   }
   
   return(modelList)
