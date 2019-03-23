1e8ee1ab7148a0581f701f7e882505917887e7c5
Yujie Wang <yujiewang1110@gmail.com>
2019-03-23 18:05:13 -0400

    save

--- lib/cv_lightGBM.R
+++ lib/cv_lightGBM.R
@@ -2,12 +2,12 @@
 ### Cross Validation for light GBM ###
 ######################################
 
-cv.function <- function(X.train, y.train, d, nr, K){
+cv.lgb <- function(X.train, y.train, d, nr, K){
   
   n <- dim(y.train)[1]
   n.fold <- floor(n/K)
   s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
-  cv.error <- rep(NA, K)
+  cv.lgb.error <- rep(NA, K)
   
   for (i in 1:K){
     train.data <- X.train[s != i, ,]
@@ -17,9 +17,19 @@ cv.function <- function(X.train, y.train, d, nr, K){
     
     par <- list(depth=d, nr = nr)
     fit <- train.lgb(train.data, train.label, par)
-    #pred <- test(fit, test.data)  
-    cv.error[i] <- mean((pred - test.label)^2)  
+    pred <- test.lgb(fit, test.data)  
+    cv.lgb.error[i] <- mean((pred - test.label)^2)  
     
   }			
-  return(c(mean(cv.error),sd(cv.error)))
+  return(c(mean(cv.lgb.error),sd(cv.lgb.error)))
 }
+
+
+nrseq <- seq(200, 200, 10)
+err_cv_lgb <- cbind(nrseq, array(dim=c(length(nrseq), 2)))
+colnames(err_cv_lgb) <- c("Num_iterations", "mean_cv_error", "sd_cv_error")
+for (i in 1:length(nrseq)){
+  err_cv_lgb[i,] <- cv.lgb(dat_train$feature, dat_train$label, nr = nrseq[i], d = 50, K = 5)
+}
+
+save(err_cv_lgb, file="../output/err_cv_lgb.RData")
